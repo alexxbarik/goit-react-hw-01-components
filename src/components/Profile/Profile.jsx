01a8d8323294e0f7b62f659358b4a3e2b5364fe8
Oleksandr Barabash <alexxbarik@gmail.com>
2023-05-17 22:38:22 +0300

    добавляє класснейм та інтерполяцію до файлу pfile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,40 +1,40 @@
 // import css from "./Profile.module.css";
 import PropTypes from "prop-types";
 
- export const Profile = ({username, tag, location, avatar, followers, views, likes}) => {
-    return (<div class="profile">
-    <div class="description">
+ export const Profile = ({username, tag, location, avatar, stats}) => {
+    return (<div className={CSS.profile}>
+    <div className={CSS.description}>
       <img
         src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
         alt="User avatar"
-        class="avatar"
+        className={CSS.avatar}
       />
-      <p class="name">Petra Marica</p>
-      <p class="tag">@pmarica</p>
-      <p class="location">Salvador, Brasil</p>
+      <p className={CSS.name}>{username}</p>
+      <p className={CSS.tag}>@{tag}</p>
+      <p className={CSS.location}>{location}</p>
     </div>
   
-    <ul class="stats">
+    <ul className={CSS.stats}>
       <li>
-        <span class="label">Followers</span>
-        <span class="quantity">1000</span>
+        <span className={CSS.label}>Followers{stats.Followers}</span>
+        <span className={CSS.quantity}>1000</span>
       </li>
       <li>
-        <span class="label">Views</span>
-        <span class="quantity">2000</span>
+        <span className={CSS.label}>Views{stats.Views}</span>
+        <span className={CSS.quantity}>2000</span>
       </li>
       <li>
-        <span class="label">Likes</span>
-        <span class="quantity">3000</span>
+        <span className={CSS.label}>Likes{stats.Likes}</span>
+        <span className={CSS.quantity}>3000</span>
       </li>
     </ul>
   </div>);
 };
 
-    Profile.propTypes = {username: PropTypes.string.isRequired, 
+    Profile.propTypes = {
+    username: PropTypes.string.isRequired, 
     tag: PropTypes.string.isRequired, 
     location: PropTypes.string.isRequired, 
     avatar: PropTypes.string.isRequired, 
-    followers: PropTypes.number.isRequired, 
-    views: PropTypes.number.isRequired, 
-    likes: PropTypes.number.isRequired};
+    stats: PropTypes.objectOf(PropTypes.number).isRequired, 
+    };
