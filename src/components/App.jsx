5c9d486e66ca2a9b007affa2bc9daf8a7c7be32c
Oleksandr Barabash <alexxbarik@gmail.com>
2023-05-22 09:47:08 +0300

    Оновлює App.jsx по зауваженням ментора

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,7 @@ export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
+        // height: '100vh',
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
